3039ea105b363893ad954b82d8295c2b675e6faf
Dinar699 <d.siraziev88@gmail.com>
2023-05-25 12:32:22 +0300

    3 commit

---
+++ src/main/java/com/example/spring_security/onStart/OnStart.java
@@ -0,0 +1,46 @@
+package com.example.spring_security.onStart;
+
+import com.example.spring_security.models.Role;
+import com.example.spring_security.models.User;
+import com.example.spring_security.service.RoleServiceImpl;
+import com.example.spring_security.service.UserService;
+import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class OnStart {
+    private final UserService userService;
+    private final RoleServiceImpl roleService;
+
+    public OnStart(UserService userService, RoleServiceImpl roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @PostConstruct
+    private void dataBaseInit() {
+        Set<Role> adminRole = new HashSet<>();
+        Role roleUser = new Role("ROLE_USER");
+        Role roleAdmin = new Role("ROLE_ADMIN");
+
+        adminRole.add(roleAdmin);
+        adminRole.add(roleUser);
+
+        Set<Role> userRole = new HashSet<>();
+        userRole.add(roleUser);
+
+        roleService.saveRole(roleAdmin);
+        roleService.saveRole(roleUser);
+
+        User admin = new User("admin", "admin", 25, "admin@mail.ru", "admin", adminRole);
+        User user = new User("user", "user", 27, "user@mail.ru", "user", userRole);
+        User tester = new User("test", "test", 32, "test@mail.ru", "test");
+
+        tester.setRoles(adminRole);
+        userService.add(admin);
+        userService.add(user);
+        userService.add(tester);
+    }
+}
